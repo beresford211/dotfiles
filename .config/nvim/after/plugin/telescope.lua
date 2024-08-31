47783a58385d7da6e4e0094f567f14b87f13a791
Tim <beresford211@users.noreply.github.com>
2024-08-31 15:57:22 -0500

    Update telescope.lua

---
+++ .config/nvim/after/plugin/telescope.lua
@@ -0,0 +1,17 @@
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+require("telescope").setup({
+  defaults = {
+    layout_strategy='vertical',
+    layout_config = {
+      vertical = { width = 0.75 },
+      mirror = true
+      -- other layout configuration here
+    },
+  },
+})
