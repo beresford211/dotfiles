7daa2430cf155526e49c8d87068b72502fb8033c
Tim <beresford211@users.noreply.github.com>
2024-08-31 16:01:53 -0500

    Update packer.lua

---
+++ .config/nvim/lua/dophs/packer.lua
@@ -0,0 +1,53 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.2',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
+
+    use({ 'rose-pine/neovim', as = 'rose-pine' })
+
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },             -- Required
+            { 'williamboman/mason.nvim' },           -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },     -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' },     -- Required
+        }
+    }
+
+    -- prettier
+    use('neovim/nvim-lspconfig')
+    use('jose-elias-alvarez/null-ls.nvim')
+    use('MunifTanjim/prettier.nvim')
+
+    use("mbbill/undotree")
+    use("tpope/vim-fugitive")
+    use 'nvim-lualine/lualine.nvim'
+
+    use("neanias/everforest-nvim")
+
+    use('lewis6991/gitsigns.nvim')
+    use('numToStr/Comment.nvim')
+    use({
+        "kylechui/nvim-surround",
+        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+    })
+end)
